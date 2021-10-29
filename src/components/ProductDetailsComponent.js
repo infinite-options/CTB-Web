9da7625230c4a4ca4f9e6d4c2cadad62252fd044
Vishal <annamaneni.v@northeastern.edu>
2021-10-29 16:18:04 -0700

    implemented nav

--- src/components/ProductDetailsComponent.js
+++ src/components/ProductDetailsComponent.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+import {GET_PRODUCT_API_URL} from "../common/constants";
+import { useEffect, useState } from "react";
 
 export default function ProductDetailsComponent (props)  {
 
@@ -7,9 +10,67 @@ export default function ProductDetailsComponent (props)  {
     const location = useLocation();
     console.log(location)
 
+    useEffect(() => {
+        getProductDetails(location.state.product_uid)
+    }, []);
+
+    const [productDetails, setProductDetails] = useState(null);
+    const [bom, setBom] = useState(null);
+
+
+
+    const getProductDetails = (productId) => {
+        axios.get(`${GET_PRODUCT_API_URL}/${productId}`)
+            .then((response) =>{
+                console.log('***')
+                console.log(response);
+                let bom_json = JSON.parse(response.data[0].product_BOM)
+                bom_json.map(row => {
+                    console.log(row)
+                })
+                setProductDetails(response.data[0])
+                setBom(bom_json)
+                // this.setState({
+                //     productDetails: response.data[0],
+                //     bom: bom_json
+                // })
+            }, (error) => {
+                console.log(error);
+            })
+    }
+
 
     return (
         <div>
             {location.state.product_uid}
+
+            { productDetails && bom && <div>
+                <h1>BOM: { productDetails.product_uid}</h1>
+                <table className="table">
+                    <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">Level</th>
+                        <th scope="col">Part Number</th>
+                        <th scope="col">Part</th>
+                        <th scope="col">Quantity</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    {
+                        bom.map((row, index) =>
+                            <tr key={index}>
+                                <th scope="row">{row['Level']}</th>
+                                <td>{row['PN']}</td>
+                                <td>{row['PN']}</td>
+                                <td>{row['Qty']}</td>
+                            </tr>
+                        )}
+
+                    </tbody>
+                </table>
+
+
+            </div>}
     </div>)
 }
