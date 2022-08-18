89ad13429973cdaa6708548cc8a2483e8d8e01b3
aSandhu7893 <anu.sandhu7893@gmail.com>
2022-08-18 02:35:21 -0700

    made page responsive

--- src/Home.jsx
+++ src/Home.jsx
@@ -21,6 +21,10 @@ export default function Home() {
         position: "absolute",
         zIndex: 1,
         flexDirection: "column",
+        maxWidth: "100%",
+        margin: 0,
+        padding: 0,
+        overflowX: "hidden",
       }}
     >
       <Container fluid>
@@ -29,153 +33,59 @@ export default function Home() {
             position: "relative",
           }}
         >
-          {/* <video
-          autoPlay={true}
-          loop={true}
-          controls={true}
-          playsInline
-          muted
-          src="Landing1Video.mov"
-          type="video/mp4"
-        /> */}
           <HomepageNavbar />
-          <div class="boxS">
-            <img src={Landing1} id="video" />
-          </div>
+
+          <img src={Landing1} id="video" />
 
           <div
-            class="header-on-image"
+            className="header-on-image"
             style={{
               display: "flex",
               flexDirection: "column",
               justifyContent: "center",
-              marginLeft: "2rem",
             }}
           >
-            <div
-              style={{
-                font: "normal normal normal 80px Montserrat-Medium",
-                marginTop: "2rem",
-              }}
-            >
-              {" "}
-              THE FUTURE OF MANUFACTURING PLANNING
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                lineHeight: 1.5,
-                font: "normal normal normal 28px Montserrat-Regular",
-              }}
-            >
-              {" "}
+            <div className="font80"> THE FUTURE OF MANUFACTURING PLANNING</div>
+            <div className="font28">
               SIMPLIFYING BUILD READINESS <br /> ORGANIZING QUALITY DATA
               <br /> PROVIDING REAL TIME METRICS
             </div>
           </div>
         </Row>
-        <Row
-          style={{
-            display: "flex",
-            backgroundColor: "black",
-            color: "white",
-            boxShadow: "0px 0px 10px 10px #000",
-          }}
-        >
-          <Col
-            xs={6}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              marginLeft: "2rem",
-            }}
-          >
-            <div
-              style={{
-                font: "normal normal normal 28px Montserrat-Medium",
-                lineHeight: 5,
-              }}
-            >
-              VISION
-            </div>
+        <Row className="blkBg">
+          <Col xs={12} sm={12} md={12} lg={4} xl={6} className="colStyle">
+            <div className="font28">VISION</div>
 
-            <div
-              style={{
-                font: "normal normal normal 34px Montserrat-Medium",
-                lineHeight: 1.6,
-              }}
-            >
+            <div className="font34">
               We’re Changing the Way the World Thinks About Manufacturing{" "}
             </div>
             <br />
             <br />
             <br />
-            <div
-              style={{
-                font: "normal normal normal 22px Montserrat-Regular",
-                lineHeight: 1.6,
-              }}
-            >
+            <div className="font22">
               Planning a build doesn't need to be complicated or time consuming.
               We simplified the process so you can find out what you need to
               order is seconds and track progress throughout the build cycle.
             </div>
           </Col>
-          <Col xs={6}>
+          <Col xs={12} sm={12} md={12} lg={8} xl={6}>
             <img src={Landing2} id="colImg" />
           </Col>
         </Row>
-        <Row
-          style={{ display: "flex", backgroundColor: "white", color: "black" }}
-        >
-          <Col
-            xs={6}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              marginLeft: "2rem",
-            }}
-          >
-            <div
-              style={{
-                font: "normal normal normal 22px Montserrat-Regular",
-                lineHeight: 3,
-              }}
-            >
-              SERVICES{" "}
-            </div>
+        <Row className="whtBg">
+          <Col xs={12} sm={12} md={12} lg={4} xl={6} className="colStyle">
+            <div className="font28">SERVICES </div>
 
-            <div
-              style={{
-                font: "normal normal normal 28px Montserrat-Medium",
-                lineHeight: 2,
-              }}
-            >
+            <div className="font28">
               We Deliver Exceptional Web-Based Software Products and Services
               Around the World
             </div>
             <br />
             <br />
 
-            <div
-              style={{
-                font: "normal normal normal 44px Montserrat-Medium",
-                lineHeight: 2,
-              }}
-            >
-              BUILD PLANNING
-            </div>
+            <div className="font44">BUILD PLANNING</div>
             <br />
-            <div
-              style={{
-                font: "normal normal normal 22px Montserrat-Regular",
-                lineHeight: 1.6,
-              }}
-            >
+            <div className="font22">
               Save time and money by ordering only what you need given existing
               inventory and current orders. Use simulations to identify problem
               areas, order buffer stock, and mitigate risks.
@@ -183,107 +93,45 @@ export default function Home() {
             <br />
             <br />
           </Col>
-          <Col xs={6}>
+          <Col xs={12} sm={12} md={12} lg={8} xl={6}>
             <img src={Landing3} id="colImg" />
           </Col>
         </Row>
-        <Row
-          style={{ display: "flex", backgroundColor: "white", color: "black" }}
-        >
-          <Col xs={6}>
+        <Row className="whtBg">
+          <Col xs={12} sm={12} md={12} lg={4} xl={6}>
             <img src={Landing4} id="colImg" />
           </Col>
-          <Col
-            xs={6}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              marginRight: "2rem",
-              marginTop: "-2rem",
-            }}
-          >
-            <div
-              style={{
-                font: "normal normal normal 44px Montserrat-Medium",
-                lineHeight: 2,
-              }}
-            >
-              REAL-TIME INFORMATION
-            </div>
-            <div
-              style={{
-                font: "normal normal normal 22px Montserrat-Regular",
-                lineHeight: 1.6,
-              }}
-            >
+          <Col xs={12} sm={12} md={12} lg={8} xl={6} className="colStyle">
+            <div className="font44">REAL-TIME INFORMATION</div>
+            <div className="font22">
               Know what you need, when you need it and where you need it in real
               time. See updates as material is checked in, quality inspections
               pass and sub-assemblies are completed.
             </div>
           </Col>
         </Row>
-        <Row
-          style={{ display: "flex", backgroundColor: "white", color: "black" }}
-        >
-          <Col
-            xs={6}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              marginLeft: "2rem",
-            }}
-          >
-            <div
-              style={{
-                font: "normal normal normal 44px Montserrat-Medium",
-                lineHeight: 1,
-              }}
-            >
-              INTEGRATED QUALITY
-            </div>
+        <Row className="whtBg">
+          <Col xs={12} sm={12} md={12} lg={4} xl={6} className="colStyle">
+            <div className="font44">INTEGRATED QUALITY</div>
             <br />{" "}
-            <div
-              style={{
-                font: "normal normal normal 22px Montserrat-Regular",
-                lineHeight: 1.6,
-              }}
-            >
+            <div className="font22">
               Put Quality First by integrating inspection results and quality
               checks throughout the ordering and manufacturing process.
               Integrate with vendor QA reports and enable visibility across your
               organization.
             </div>
           </Col>
-          <Col xs={6}>
+          <Col xs={12} sm={12} md={12} lg={8} xl={6}>
             <img src={Landing5} id="colImg" />
           </Col>
         </Row>
         <Row id="parallax">
-          <div
-            class="text-on-image"
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              marginLeft: "2rem",
-            }}
-          >
-            <div
-              style={{
-                font: "normal normal normal 22px Montserrat-Regular",
-                lineHeight: 3,
-              }}
-            >
-              WHY BUILD SUCCESS
-            </div>
-            <div
-              style={{
-                font: "normal normal normal 34px Montserrat-Medium",
-                lineHeight: 1.2,
-              }}
-            >
+          <div className="text-on-image">
+            <div className="font34">WHY BUILD SUCCESS</div>
+            <br />
+            <br />
+            <br />
+            <div className="font34">
               A different approach.
               <br />A different thought process.
               <br /> A better result.
@@ -292,40 +140,19 @@ export default function Home() {
             <br />
             <br />
             <br />
-            <div
-              style={{
-                font: "normal normal normal 22px Montserrat-Regular",
-                lineHeight: 1.6,
-              }}
-            >
+            <div className="font22">
               Why waste resources planning and executing a build? Track
               everything in one location from ordering to shipping
             </div>
           </div>
         </Row>
-        <Row
-          style={{ display: "flex", backgroundColor: "white", color: "black" }}
-        >
-          <Col>
+        <Row className="whtBg">
+          <Col xs={12} sm={12} md={12} lg={4} xl={6}>
             <img src={Landing7} id="colImg" />
           </Col>
 
-          <Col
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              marginLeft: "2rem",
-            }}
-          >
-            <div
-              style={{
-                font: "normal normal normal 22px Montserrat-Regular",
-                lineHeight: 1.6,
-              }}
-            >
-              Build Success in Numbers
-            </div>
+          <Col xs={12} sm={12} md={12} lg={8} xl={6} className="colStyle">
+            <div className="font22">Build Success in Numbers</div>
           </Col>
         </Row>
       </Container>
--- src/HomepageNavbar.jsx
+++ src/HomepageNavbar.jsx
@@ -1,56 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Container } from "react-bootstrap";
+import MenuIcon from "./Images/MenuIcon.svg";
 import "./home.css";
 
-const HomepageNavbar = () => (
-  <nav class="headerNav">
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        width: "100%",
-      }}
-    >
+export default function HomepageNavbar() {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    console.log("here handleclick");
+    setOpen(!open);
+  };
+
+  const closeMenu = () => {
+    setOpen(false);
+  };
+
+  const closeMenuProjects = () => {
+    setOpen(false);
+  };
+
+  return (
+    <nav class="headerNav">
       <div
-        xs={6}
         style={{
           display: "flex",
           flexDirection: "row",
           width: "100%",
         }}
       >
-        <Link to="/" class="logo">
-          BUILD SUCCESS
-        </Link>
+        <div
+          xs={6}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+          }}
+        >
+          <Link to="/" class="logo" onClick={closeMenu}>
+            BUILD SUCCESS
+          </Link>
+        </div>
+        <div>
+          {open ? (
+            <img
+              src={MenuIcon}
+              className="nav-icon"
+              onClick={() => {
+                handleClick();
+              }}
+            />
+          ) : (
+            <img
+              src={MenuIcon}
+              className="nav-icon"
+              onClick={() => {
+                handleClick();
+              }}
+            />
+          )}
+        </div>
+        <div className={open ? "nav-links active" : "nav-links"}>
+          <Link to="/" class="navButtons" onClick={closeMenuProjects}>
+            Technology
+          </Link>
+          <Link to="/buyparts" class="navButtons" onClick={closeMenuProjects}>
+            CTB
+          </Link>
+          <Link to="/addparts" class="navButtons" onClick={closeMenuProjects}>
+            Add Parts
+          </Link>
+          <Link to="/inventory" class="navButtons" onClick={closeMenuProjects}>
+            Inventory
+          </Link>
+          <Link to="/editpart" class="navButtons" onClick={closeMenuProjects}>
+            Edit Parts
+          </Link>
+        </div>
       </div>
-
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          width: "100%",
-          justifyContent: "flex-end",
-        }}
-      >
-        <Link to="/" class="navButtons">
-          Technology
-        </Link>
-        <Link to="/buyparts" class="navButtons">
-          CTB
-        </Link>
-        <Link to="/addparts" class="navButtons">
-          Add Parts
-        </Link>
-        <Link to="/inventory" class="navButtons">
-          Inventory
-        </Link>
-        <Link to="/editpart" class="navButtons">
-          Edit Parts
-        </Link>
-      </div>
-    </div>
-  </nav>
-);
-
-export default HomepageNavbar;
+    </nav>
+  );
+}
