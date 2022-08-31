2e29d4f8aeb0520ad347ced58ebb45ff14a20ef5
Viha123 <sviha195@gmail.com>
2022-08-31 15:27:22 -0700

    update tech page

--- src/technology.js
+++ src/technology.js
@@ -3,8 +3,13 @@ import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import Home from "./Home";
 import HomepageNavbar from "./HomepageNavbar";
-import techImg1another from "./Images/techImg1another.webp"
+import techImg1another from "./Images/techImg1.webp"
 import techImg2 from "./Images/techImg2.webp"
+import bomImg1 from "./Images/updatedSizeBom1.webp"
+import bomImg2 from "./Images/updatedSizeBom2.webp"
+import bomImg3 from "./Images/updatedSizeBom3.webp"
+import rightArrow2 from "./Images/rightArrow2.PNG"
+import rightArrow1 from "./Images/rightArrow1.PNG"
 
 import "./tech.css"
 export default function Technology(){
@@ -22,27 +27,38 @@ export default function Technology(){
                         alignItems: "center",
                     }}>
                         <div className="font80 title">BUILD READINESS</div>
-                        <div className="font28">Build Success starts with the right materials.  <br/>Upload your Bill of Materials (BOM) and let's get started</div>
+                        <div className="font28 subTitle">Build Success starts with the right materials.  <br/>Upload your Bill of Materials (BOM) and let's get started</div>
 
                     </div>
 
                 </Row>
                 <Row className ="bomPage1">
                     {/* <img src={techImg1another} id="techImg1" /> */}
-                    <div className = "page2" style = {{
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: "space-around",
-                        marginTop: "1rem",
-                    }}>
+                    
+                    <div className = "page2" >
 
-                        <div className = "font34 col1"> Upload your BOM</div>
-                        <div className = "font34 col2">And instantly see your Order List</div>
+                        <div className = "font34 col1 top"> Upload your BOM</div>
+                        <div className = "font34 col2 top">And instantly see your Order List</div>
+                        <div className="left">
+                            <img width="592px" height="337px"src ={bomImg1} className = "bom1"></img>
+                            <img src ={rightArrow1} className = "arrow1"></img>
+                        </div>
+                        
+                        <div className = "mid">
+                            <img width="240px" height="823px" src ={bomImg2} className = "bom2"></img>
+                            <img src ={rightArrow2} id = "arrow2"></img>
+                        </div>
+                        
+                        <img width="675px" height="288px" src ={bomImg3} id = "bom3"></img>
+                        
+                        
                     </div>
+                    <button className="button">How we do it</button>
 
                 </Row>
                 <Row className ="bomPage1">
-                    {/* <img src={techImg1another} id="techImg1" /> */}
+                    <div height="1000px">nothing here</div>
+                    {/* <img src={techImg1another} id="techImg1" />
                     <div className = "page2" style = {{
                         display: "flex",
                         flexDirection: "row",
@@ -59,7 +75,7 @@ export default function Technology(){
                         </div>
                         <img src={techImg2} className="right-picture-col"/> 
 
-                    </div>
+                    </div> */}
 
                 </Row>
 
